d0f721e9b80093529a9dad16af6a6095391aed8b
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-02-26 16:27:23 +0530

    .

--- js/index.js
+++ js/index.js
@@ -2,9 +2,24 @@ burger = document.querySelector('.burger');
 header = document.querySelector('header');
 nav = document.querySelector('nav');
 
-
+// height and nav visiblity
 burger.addEventListener('click', () => {
     header.classList.toggle('headerheight');
     nav.classList.toggle('navvisible');
 });
+
+//burger animation
+let showMenu = false;
+burger.addEventListener("click", toggleMenu);
+function toggleMenu() {
+    if (!showMenu) {
+        burger.classList.add("close");
+        // Reset the menu state
+        showMenu = true;
+    } else {
+        burger.classList.remove("close");
+        // Reset the menu state
+        showMenu = false;
+    }
+}
 
